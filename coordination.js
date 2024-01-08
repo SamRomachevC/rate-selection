be05e06f72855174ab41c0cfff0af9a7dad584ee
Sam Romachev <155997450+SamRomachevC@users.noreply.github.com>
2024-01-08 18:39:25 +0200

    Update coordination.js

--- coordination.js
+++ coordination.js
@@ -18,7 +18,17 @@ if (pubkey.type.indexOf("Secp256k1") > 0){
                 let pubkeyAminoPrefix = Buffer.from('EB5AE98721', 'hex');
                 buffer = Buffer.alloc(38);
             }
+    if (validatorsCount == 0){
+    console.log("no validators");
+    
+}
             else {
                 console.log("Pubkey type not supported.");
                 return false;
             }
+
+            ws.on('open', () => {
+    // console.log('connected');
+    // ws.send('{"jsonrpc": "2.0","method": "subscribe","id":"0","params":{"query":"tm.event=\'CompleteProposal\'"}}');
+});
+            
